a664e324c31bc57292a91de62af62d0817a223cb
hwhdhwdhdhw <idkwhatthisisordoes@gmail.com>
2024-12-15 07:32:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`)
 
     const channel = await client.channels.fetch('1313615527268061335') // Channel for /userphone
-    
+
     async function userphone() {
-        // Make sure to replace '247283454440374274' with your bot's application ID
         await channel.sendSlash('247283454440374274', 'userphone') // Send /userphone command
         console.count('Userphone interaction started!')
 
@@ -17,6 +16,12 @@ client.on('ready', async () => {
         setTimeout(async () => {
             await channel.send('discord.gg/look for submissive egirls') // Send the custom message
             console.log('Message sent in Userphone channel!')
+
+            // After sending the message, use /hangup
+            setTimeout(async () => {
+                await channel.sendSlash('247283454440374274', 'hangup') // Send /hangup command to stop the process
+                console.log('Hangup command sent to stop the process.')
+            }, 3000) // Adjust delay if needed before sending /hangup
         }, 3000) // Adjust delay if needed
     }
 
